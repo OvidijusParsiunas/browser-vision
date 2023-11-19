032946333ce0353434fb62a3586e9610f2b9e37d
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2023-11-19 23:36:44 +0000

    ability to share data across tabs

--- popup.js
+++ popup.js
@@ -1,36 +1,81 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('browser-vision-capture').addEventListener('click', captureScreenshot);
-  document.getElementById('browser-vision-send').addEventListener('click', sendRequest);
+  const config = processConfig();
+  setUpTask(config);
+  setUpCapture(config);
+  setUpRequest(config);
 });
 
 // WORK - ability to remove the icon
-let images = [];
-const allImages = [];
+// WORK - prevent multiple auto capture sessions
+// use another storage item?????
+function processConfig() {
+  if (!localStorage.getItem('browser-vision')) {
+    const defaultConfig = {images: [], allImages: [], task: 'Tell me what you think about this image'};
+    localStorage.setItem('browser-vision', JSON.stringify(defaultConfig));
+  }
+  return JSON.parse(localStorage.getItem('browser-vision'));
+}
 
-async function captureScreenshot() {
-  console.log('capturing image');
-  const image = await chrome.tabs.captureVisibleTab(null, {format: 'png'});
-  console.log('image captured');
+function setUpTask(config) {
+  const taskEl = document.getElementById('browser-vision-task');
+  taskEl.addEventListener('keyup', changeTask.bind(this, config));
+  taskEl.value = config.task;
+}
+
+function changeTask(config, event) {
+  config.task = event.target.value;
+  localStorage.setItem('browser-vision', JSON.stringify(config));
+}
+
+function setUpCapture(config) {
+  const gallery = setUpGallery(config);
+  const captureButton = document.getElementById('browser-vision-capture');
+  captureButton.addEventListener('click', captureScreenshot.bind(this, config, gallery));
+}
+
+function setUpGallery(config) {
   const gallery = document.getElementById('browser-vision-images');
   gallery.style.overflow = 'auto';
   gallery.style.maxHeight = '100px';
+  config.images.forEach((image) => {
+    addImage(gallery, image);
+  });
+  return gallery;
+}
+
+function addImage(gallery, image) {
   const imageIcon = document.createElement('img');
   imageIcon.style.width = '80px';
   imageIcon.style.height = '40px';
   imageIcon.style.border = '1px solid grey';
   imageIcon.style.borderRadius = '8px';
   imageIcon.src = image;
-  images.push(image);
-  allImages.push(image);
   gallery.appendChild(imageIcon);
+}
+
+async function captureScreenshot(config, gallery) {
+  console.log('capturing image');
+  const image = await chrome.tabs.captureVisibleTab(null, {format: 'png'});
+  console.log('image captured');
+  config.images.push(image);
+  config.allImages.push(image);
+  addImage(gallery, image);
   scrollToBottom(gallery);
+  setTimeout(() => {
+    localStorage.setItem('browser-vision', JSON.stringify(config));
+  });
 }
 
 function scrollToBottom(element) {
   element.scrollTop = element.scrollHeight;
 }
 
-async function sendRequest() {
+function setUpRequest(config) {
+  const sendButton = document.getElementById('browser-vision-send');
+  sendButton.addEventListener('click', sendRequest.bind(this, config));
+}
+
+async function sendRequest(config) {
   console.log('requesting response');
   try {
     const result = await fetch('https://api.openai.com/v1/chat/completions', {
@@ -39,33 +84,36 @@ async function sendRequest() {
         'Content-Type': 'application/json',
         Authorization: '',
       },
-      body: buildBody(),
+      body: buildBody(config),
     });
     const responseData = await result.json();
     console.log(responseData);
     const resultText = responseData.choices[0].message.content;
     const resultElement = document.getElementById('browser-vision-result');
     resultElement.value = resultText;
-    images = [];
+    config.images = [];
     const gallery = document.getElementById('browser-vision-images');
     gallery.replaceChildren();
+    setTimeout(() => {
+      localStorage.setItem('browser-vision', JSON.stringify(config));
+    });
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
-function buildBody() {
+function buildBody(config) {
   const body = {
     model: 'gpt-4-vision-preview',
     max_tokens: 300, // otherwise AI does not return full responses - remove when this behaviour changes
     messages: [
       {
         role: 'system',
-        content: 'Tell me what you think about this image',
+        content: localStorage.getItem('browser-vision-task'),
       },
     ],
   };
-  const imageContent = images.map((image) => {
+  const imageContent = config.images.map((image) => {
     return {type: 'image_url', image_url: {url: image}};
   });
   if (imageContent.length > 0) body.messages.push({role: 'user', content: imageContent});
